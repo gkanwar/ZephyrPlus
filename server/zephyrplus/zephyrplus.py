7b3a606d069d80022602a1878296aa9ea3ab42d4
Michael Wu <mikewu13@gmail.com>
2012-03-14 16:10:33 -0400

    ZephyrPlus.py modifications for receiving zephyrs

--- server/zephyrplus/zephyrplus.py
+++ server/zephyrplus/zephyrplus.py
@@ -2,6 +2,7 @@ import tornado.httpserver
 import tornado.ioloop
 import tornado.web
 
+
 import os
 import time
 import datetime
@@ -11,6 +12,10 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
 #import models
 from models import Zephyr, Subscription, Account
+
+# signals for notification
+from django.dispatch import receiver
+import django.db.models.signals
 
 #from django.conf import settings
 #settings.configure(DATABASE_ENGINE='sqlite3', DATABASE_NAME='zephyrs.db')
@@ -27,24 +32,38 @@ class MessageWaitor(object):
 	def wait_for_messages(cls, request, sub):
 		cls.waiters.append((request, sub))
 
-	def new_messages(cls, zephyr):
+	@classmethod
+	def new_message(cls, zephyr):
 		for waitee in cls.waiters:
 			if waitee[1].match(zephyr):
-				waitee[0].write(zephyr)
+				response = []
+				values = {
+						'message': zephyr.message,
+						'sender': zephyr.sender,
+						'date': (zephyr.date - datetime.datetime.fromtimestamp(0)).total_seconds()*1000,
+						'class': zephyr.dst.class_name,
+						'instance': zephyr.dst.instance,
+						'recipient': zephyr.dst.recipient
+						}
+				response.append(values)
+				waitee[0].set_header('Content-Type', 'text/plain')
+				waitee[0].write(simplejson.dumps(response))
 				waitee[0].finish()
-				waiter.remove(callback)
-				
+				cls.waiters.remove(waitee)
+
 class ChatUpdateHandler(tornado.web.RequestHandler):
 	@tornado.web.asynchronous
 	def get(self, *args, **kwargs):
 		class_name=self.get_argument('class')
 		instance = self.get_argument('instance', "*")
 		recipient = self.get_argument('recipient', "*")
-		startdate = self.get_argument('startdate', "0") # change later to 2 weeks ago
+		startdate = self.get_argument('statedate', time.time()*1000)
+		#startdate = self.get_argument('startdate', "0") # change later to 2 weeks ago
 		enddate = self.get_argument('enddate', str(1000*2**35)) # if longpolling, should not have end date
 		longpoll = self.get_argument('longpoll', "False")
 		#TODO: do input validation on arguments
 		sub = Subscription(class_name=class_name, instance=instance, recipient=recipient)
+		print('\n');
 		print sub
 		print sub.get_filter()
 		print "startdate:" + str(datetime.datetime.fromtimestamp(float(startdate)/1000))
@@ -85,8 +104,16 @@ class ChatUpdateHandler(tornado.web.RequestHandler):
 		proc.stdin.write(message)
 		proc.stdin.close()
 
+class NewZephyrHandler(tornado.web.RequestHandler):
+	def get(self, *args, **kwargs):
+		z_id = self.get_argument('id', default=0);
+		if z_id != None and z_id > 0:
+			z = Zephyr.objects.filter(id=z_id)
+			MessageWaitor.new_message(z[0])
+
 application = tornado.web.Application([
 	(r"/chat", ChatUpdateHandler),
+	(r"/update", NewZephyrHandler),
 	(r"/", MainPageHandler),
 		])
 
