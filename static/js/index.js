42d15a4993a34f2f8323f18156747894b19d3498
Michael Wu <mikewu13@gmail.com>
2012-04-19 15:55:16 -0400

    Fixing classes being added twice.
    addZephyrClass was pushing to api.classes, but did not have
    all the fields such as instanceDict and also did not add the
    subscription to the classDict.
    
    Deleted this code from addZephyrClass, the addSubscription takes care of
    creating the class correctly and adding it to api.classDict.
    
    The class sidebar does not display correctly, but the internal state is
    now correct for adding classes.
    
    Also handling if Add Class is cancelled, cleanly returns.

--- static/js/index.js
+++ static/js/index.js
@@ -598,18 +598,13 @@ var fillInstancesDropDown = function(instance_id)
 var addZephyrClass = function()
 {
     var new_class_name = prompt("Please enter the class you want to add.");
+    if(new_class_name == undefined){
+        return;
+    }
+
     new_class_name = new_class_name.replace(/^\s+|\s+$/g, '');
     if(new_class_name != "" && api.classDict[new_class_name] == undefined) {
         api.addSubscription(new_class_name);
-        api.classes.push(
-                {
-                    id:api.classes.length,
-            name: new_class_name,
-            last_messaged: null,
-            color: hashStringToColor(new_class_name),
-            instances: [],
-            messages: []
-                });
         fillClasses();
     }
 };
