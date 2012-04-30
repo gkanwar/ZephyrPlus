0e3650b2eaacf0a2b1740e26aabc3a6cd67f8ef5
Michael Wu <mikewu13@gmail.com>
2012-04-30 01:39:20 -0400

    Prevent wildcard expansion in insert_zephyr.sh
    Adding a set -f, which should prevent wildcard expansion

--- insert_zephyr.sh
+++ insert_zephyr.sh
@@ -4,7 +4,7 @@
 # Script Arugments:
 # (1) class, (2) instance, (3) recipient, (4) sender, (5) body, (6) signature
 
-
+set -f
 
 PIPE="incomingZephyrs.pipe"
 LOCK="$PIPE.lock"
@@ -23,12 +23,14 @@ fi
 #lock the file or wait (up to forever) until aquisition of lock
 #echo "trying to acquire lock"
 flock -x $LOCK -c "(
+	set -f;
 	echo $1 > $PIPE;
 	echo $2 > $PIPE;
 	echo $3 > $PIPE;
 	echo $4 > $PIPE;
 	echo $5 > $PIPE;
 	echo $6 > $PIPE;
-	echo "" > $PIPE;
+	echo  "" > $PIPE;
 	)"
+echo "$*"
 #echo "successfully sent message down the pipe"
