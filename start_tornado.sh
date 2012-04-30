d728fdf18f6e85b5ff2734b22a6397fca0a0a243
Michael Wu <mikewu13@gmail.com>
2012-04-30 01:48:45 -0400

    Adding signal traps to start scripts.
    Adding traps to start scripts so start-stop-daemon will work

--- start_tornado.sh
+++ start_tornado.sh
@@ -2,5 +2,17 @@
 LOGFILE=/var/log/tornado.log
 PYTHON=/usr/bin/python
 
+# Start the tornado server
 $PYTHON /ZephyrPlus/zephyrplus.py 2>> $LOGFILE &
+TORNADO_PID="$!"
+
+# Start the insert zephyr process
 $PYTHON /ZephyrPlus/insert_zephyr.py >> $LOGFILE 2>> $LOGFILE &
+INSERT_PID="$!"
+
+# Kill children on exit signal
+trap "(kill $TORNADO_PID; kill $INSERT_PID)" exit INT TERM
+
+# Wait for child processes to exit
+wait
+
--- zpd.sh
+++ zpd.sh
@@ -1,4 +1,13 @@
 #! /bin/bash
 LOGFILE=/var/log/zpd.log
 kinit daemon/zephyrplus.xvm.mit.edu -k -t /ZephyrPlus/zephyrplus-new-keytab
-zwgc -subfile /ZephyrPlus/.zephyrs.subs -f /ZephyrPlus/.zwgc.desc -ttymode -nofork >> "$LOGFILE"
+zwgc -subfile /ZephyrPlus/.zephyrs.subs -f /ZephyrPlus/.zwgc.desc -ttymode -nofork >> "$LOGFILE" &
+
+# Store the child PID
+CHILD="$!"
+
+# Kill chid when start-stop-daemon sends us a kill signal
+trap "kill $CHILD" exit INT TERM
+
+# Wait for child process to exit
+wait
