2ba2f51eabf9e6d103fec744ba56d3c46f4ad1e3
Michael Wu <mikewu13@gmail.com>
2012-05-04 08:05:47 -0400

    Ignoring SERVACK from zserver.
    Only allowing ACKED messages to be inserted for now, ignoring SERVACK
    messages. Other kinds of notices will be logged. If they come up, we'll
    deal with them appropriately.

--- loadZephyrs.py
+++ loadZephyrs.py
@@ -55,9 +55,13 @@ class ZephyrLoader(threading.Thread):
                 sys.exit(2)
 
 
-
     # Inserts a received ZNotice into our database
     def insertZephyr(self, zMsg):
+        # Check that the msg is an actual message and not other types.
+        if zMsg.kind != 2: # ACKED
+            if zMsg.kind != 5: #SERVACK
+                log("Recieved a " + str(zMsg.kind) + " notice")
+            return
         # Create a valid destination field for our zephyr
         s = Subscription.objects.get_or_create(class_name=zMsg.cls.lower(), instance=zMsg.instance.lower(), recipient=zMsg.recipient.lower())[0]
 
