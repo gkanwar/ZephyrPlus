96267d95b84fd721afb56faa1524f2901701a1d6
Michael Wu <mikewu13@gmail.com>
2012-05-04 19:41:02 -0400

    Repository cleanup. Removing files for zwgc.
    Removing .zwgc, zpd.sh.
    
    start_tornado has been modified to include the authentication for
    daemon.

--- start_tornado.sh
+++ start_tornado.sh
@@ -2,6 +2,9 @@
 LOGFILE=/var/log/tornado.log
 PYTHON=/usr/bin/python
 
+# Authenticate our daemon
+kinit daemon/zephyrplus.xvm.mit.edu -k -t /ZephyrPlus/zephyrplus-new-keytab
+
 # Start the tornado server
 $PYTHON /ZephyrPlus/zephyrplus.py 2>> $LOGFILE &
 TORNADO_PID="$!"
