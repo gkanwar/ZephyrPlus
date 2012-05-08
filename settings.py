ebcf5bdb39d70e119daeca6b9cec16983ed771f7
Michael Wu <mikewu13@gmail.com>
2012-05-08 09:03:35 -0400

    Adding django extensions

--- settings.py
+++ settings.py
@@ -120,6 +120,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 	'chat',
+    'django_extensions',
 )
 
 # A sample logging configuration. The only tangible logging
