72c2f70a4f9afc0f74167cc23f2fb044c2174ae1
Gary Wang <garywang@mit.edu>
2012-05-09 15:44:20 -0400

    Fix bug with clicking on an instance for the first time

--- static/js/index.js
+++ static/js/index.js
@@ -682,7 +682,7 @@ var fillMessagesByClass = function(class_id, instance_id)
 	    var selected = $("#instances_entry_id_"+instance_id);
 	    if(selected.length==0){
 		fillClasses();
-		selected = $("#instances_entry_id_"+class_id);
+		selected = $("#instances_entry_id_"+instance_id);
 	    }
 	    selected.addClass("selected");
 	    if(selected.position().top < 0 || selected.position().top > selected.offsetParent().height()-50){
