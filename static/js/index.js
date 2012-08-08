0aaaf71e180737e8a4e4173964ed6ab11715c0a7
Gary Wang <garywang@mit.edu>
2012-08-08 16:02:05 -0400

    Sort unclasses after their corresponding classes

--- static/js/index.js
+++ static/js/index.js
@@ -421,8 +421,14 @@ var fillClasses = function()
     var ul = $("<ul></ul>");
     api.classes.sort(function(c1, c2)
 		     {
-			 if (c1.name > c2.name) { return 1; }
-			 else if (c1.name < c2.name) { return -1; }
+			 var name1 = c1.name, name2 = c2.name;
+			 var un1=false, un2=false;
+			 if (name1.indexOf("un") == 0) { name1=name1.substr(2); un1=true; }
+			 if (name2.indexOf("un") == 0) { name2=name2.substr(2); un2=true; }
+			 if (name1 > name2) { return 1; }
+			 else if (name1 < name2) { return -1; }
+			 else if (un1 && !un2) { return 1; }
+			 else if (un2 && !un1) { return -1; }
 			 else { return 0; }
 		     });
     for (var i = 0; i < api.classes.length; i++)
