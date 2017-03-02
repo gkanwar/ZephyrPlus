8ad42ee99ef5a07394d188cfbece18c662b16636
Gary Wang <garywang@mit.edu>
2017-03-02 00:43:23 -0500

    Formatting

--- zephyr_utils.py
+++ zephyr_utils.py
@@ -33,13 +33,15 @@ def send_zephyr(cls, instance, recipient, sender, message, signature):
     else:
         fields = [encode(signature), encode(message)]
 
-    z = zephyr.ZNotice(cls=encode(cls),
-                       instance=encode(instance),
-                       recipient=encode(recipient),
-                       sender=encode(sender),
-                       fields=fields,
-                       format='http://zephyr.1ts.org/wiki/df',
-                       auth=False)
+    z = zephyr.ZNotice(
+        cls=encode(cls),
+        instance=encode(instance),
+        recipient=encode(recipient),
+        sender=encode(sender),
+        fields=fields,
+        format='http://zephyr.1ts.org/wiki/df',
+        auth=False
+    )
 
     _add_hmac(z)
     z.send()
@@ -67,15 +69,17 @@ def receive_zephyr():
             logger.warning(u'Failed to decode string %r' % s, exc_info=True)
             return s.decode(charset, 'replace')
 
-    return zephyr.ZNotice(uid=z.uid,
-                          kind=z.kind,
-                          auth=z.auth,
-                          cls=decode(z.cls),
-                          instance=decode(z.instance),
-                          recipient=decode(z.recipient),
-                          sender=decode(z.sender),
-                          opcode=decode(z.opcode),
-                          message=decode(z.message))
+    return zephyr.ZNotice(
+        uid=z.uid,
+        kind=z.kind,
+        auth=z.auth,
+        cls=decode(z.cls),
+        instance=decode(z.instance),
+        recipient=decode(z.recipient),
+        sender=decode(z.sender),
+        opcode=decode(z.opcode),
+        message=decode(z.message)
+    )
 
 
 def _add_hmac(z):
@@ -117,8 +121,10 @@ def _check_hmac(z):
             logger.debug('duplicate zephyr: %s', z.__dict__)
             return False
         _seen_times.add(sent_time)
-        if random.random() < 0.001 and current_time - min(_seen_times) > CLOCK_SKEW_MAX_S:
-            _seen_times = {t for t in _seen_times if current_time - t < CLOCK_SKEW_MAX_S}
+        if random.random() < 0.001 and \
+           current_time - min(_seen_times) > CLOCK_SKEW_MAX_S:
+            _seen_times = {t for t in _seen_times
+                           if current_time - t < CLOCK_SKEW_MAX_S}
 
     # Overwrite the UID because the original UID isn't authenticated.
     z.uid.address = received_hmac
@@ -136,7 +142,7 @@ def _hmac_znotice(z):
         z.sender,
         z.opcode or '',
         z.message,
-        )
+    )
 
 
 def _compute_hmac(*fields):
